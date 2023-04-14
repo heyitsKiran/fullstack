b64cbd02ea8ad04fac7f9cd47ae37fe8cad4890e
heyitsKiran <heyitskiran17@gmail.com>
2023-04-15 04:24:20 +0530

    practice

--- Java Script/hey/one.js
+++ Java Script/hey/one.js
@@ -1,3 +1,176 @@
-abc = "asdfghjkl";
-let bcd = abc.split("").reverse().join("");
-console.log(bcd);
+// abc = "asdfghjkl";
+// let bcd = abc.split("").reverse().join("");
+// console.log(bcd);
+
+// console.log(Math.sqrt(121))
+
+// let salary = 64129;
+// if (salary > 50000) {
+//   console.log("ready for travel");
+// } else {
+//   console.log("get the hell out");
+// }
+
+// var a = 50.00 > 780 ? "its correct" : "no its wrong"
+// console.log(a)
+
+// hey = ["one","two","three", 45, 16]
+// for(he of hey){
+//     console.log(he)
+// }
+
+// hey = {
+//   name: "kiran",
+//   age: 18,
+//   city: "bangalore",
+//   occ: "developer",
+// };
+
+// for (i = 0; i <= 31; i++) {
+//   console.log(i);
+// }
+
+// for (i = 15; i >= 1; i--) {
+//     console.log(i)
+// }
+
+// let i = 0;
+// while (i < 41) {
+//   if (i % 4 == 0) {
+//   }
+//   console.log(i);
+//   i++;
+// }
+
+// for (let i = 0; i <= 40; i++) {
+//   if (i % 4 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// function wish(voi) {
+//   console.log(voi,"hello world");
+// }
+// wish("wassup");
+
+// function sum(a,b){
+//   return a*b
+// }
+// console.log(sum(5012540,40))
+
+// let add=(a,b)=>a+b
+// let result= add(50,10)
+// console.log(result)
+
+//let abc = [50, 15, 14, 26, 58, 97, 45];
+// console.log(abc.length-1)
+
+// for(array of abc){
+//   console.log(array)
+// }
+
+// let lunch = [
+//   { name: "pizza", price: 200 },
+//   { name: "burger", price: 100 },
+// ];
+
+// console.log(lunch[0].price)
+
+// let array = [
+//   [4, 5, 6],
+//   [1, 2, 3],
+//   [7, 8, 9],
+// ];
+// console.log(array[1][1])
+
+// let num = [101, 102, 103, 104];
+// let i = 0;
+// while (i <= num.length - 1) {
+//   console.log(num[i]);
+//   i++;
+// }
+
+// for(no of num){
+//   console.log(no)
+// }
+
+// array = [15, 12, 42, 49, "asdfghjkl,'qwertyuiop"];
+// let ulta = (array) => {
+//   return array.reverse()
+// }
+// console.log(ulta(array))
+
+// function ulta(array) {
+//   for (let i = array.length - 1; (i >= 0); i--) {
+//     console.log(array[i])
+//   }
+// }
+// ulta(array)
+
+// array = [15, 12, 42, 49, 37];
+// console.log(Math.max(...array))
+
+// function high(array) {
+//   let max = array[0];
+//   for (let i = 0; i <= array.length - 1; i++) {
+//     if (array[i] >= max) {
+//       max = array[i];
+//     }
+//   }
+//   return max;
+// }
+// console.log(high(array));
+
+// let add = (array) => {
+//   let sum = 0;
+//   for (let i = 0; i <= array.length - 1; i++) {
+//     sum += array[i];
+//   }
+//   return sum;
+// };
+// console.log(add(array));
+
+// function add(array) {
+//   let sum = 0;
+//   for (num of array) {
+//     sum += num;
+//   }
+//   return sum;
+// }
+// console.log(add(array));
+
+// let abc = {
+//   name: "asdfghjkl",
+//   age: 3215,
+//   loc: "qwertyuiop",
+// };
+
+// let count = 0
+// for(prop in abc){
+//   ++count
+// }
+// console.log(count)
+
+// class Employee {
+//   name = "kiran";
+//   age = 45;
+//   loc = "london";
+// }
+
+// let e1 = new Employee("gokul", 27, "mambo");
+// console.log(e1)
+// console.log(Employee)
+
+class Employee {
+  constructor(name, age, loc) {
+    this.name = name;
+    this.age = age;
+    this.loc = loc;
+  }
+}
+let e1 = new Employee("gokul", 27, "mambo");
+let e2 = new Employee("kiran", 28, "bangalore");
+let e3 = new Employee("kaushik", 88, "viveknagar");
+console.log(e1);
+console.log(e2);
+console.log(e3);
