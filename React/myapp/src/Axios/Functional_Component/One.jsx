e42bacb556c01d3305d772e6b511626209255079
heyitsKiran <heyitskiran17@gmail.com>
2023-05-08 00:41:16 +0530

    Updt

--- React/myapp/src/Axios/Functional_Component/One.jsx
+++ React/myapp/src/Axios/Functional_Component/One.jsx
@@ -24,31 +24,48 @@ import { useState } from "react";
 
 const One = () => {
   let [myData, setMyData] = useState([]);
+  let [isError, setIsError] = useState(""); // NOTE : Error Handling useState
+
+  // useEffect(() => {
+  //   axios
+  //     .get("https://jsonplaceholder.typicode.com/users")
+  //     .then((response) => setMyData(response.data))
+  //     .catch((error) => setIsError(error.message)); // NOTE : Error Handling
+  // }, []);
+
+  // NOTE :  Async Await
+  let getData = async () => {
+    try {
+      let res = await axios.get("https://jsonplaceholder.typicode.cm/users");
+      setMyData(res.data);
+    } catch (error) {
+      setIsError(error.message);
+    }
+  };
 
   useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/users")
-      .then((response) => setMyData(response.data));
+    getData();
   }, []);
 
   return (
-    <div>
-      Axios using Promises
+    <div className="container bg-primary">
+      <h1 className="text-center my-3">Axios using Hooks</h1>
+      {isError !== "" && <h2 className="text-center text-danger">{isError}</h2>}
       <div className="card">
         {myData.map((user) => {
           const { id, name, email, username } = user;
           return (
-            <div className="card" key={id}>
+            <div className="card text-center bg-light" key={id}>
               <h2>{name}</h2>
-              <p>
-                <h5>
+              <h5>
+                <p>
                   ID : {id}
                   <br />
                   Email : {email}
                   <br />
                   Username : {username}
-                </h5>
-              </p>
+                </p>
+              </h5>
             </div>
           );
         })}
